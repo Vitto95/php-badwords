0e8cc64dc4957991c482ff9fb51647af70e0516a
Vitto95 <grimaldi.vitto@gmail.com>
2021-06-03 14:11:03 +0200

    prima del passaggio del valore get

--- index.php
+++ index.php
@@ -7,6 +7,27 @@
   <title>Badwords</title>
 </head>
 <body>
+<?php 
+$par = "lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rem, libero aut dicta quis dolore vel porro facere iusto, hic repudiandae voluptates esse minima aliquid odit enim molestias laboriosam nemo.";
+$badword = $_GET["badword"];
+
+?>
+
+
+  <h1>Badwords</h1>
+  <p>Paragrafo: <?php echo $par ?></p>
+  <p>Lunghezza paragrafo: <?php echo strlen($par); ?>  </p>
+  <p>Parola da censurare: <?php echo $badword ?></p>
+  <p>strpos: <?php echo strpos($par, "lorem"); ?></p>
+  <p> <?php 
+    if (strpos($par, "lorem") !== false){
+    echo str_replace("lorem", "*****", $par);
+    } else {
+    echo "NO";
+    }
+        ?>
+  
+</p>
   
 </body>
 </html>
