9d23b85876c3e6bb0a6794a895c9b2a53ccc4d70
Vitto95 <grimaldi.vitto@gmail.com>
2021-06-03 14:20:30 +0200

    parte logica completata

--- index.php
+++ index.php
@@ -7,21 +7,21 @@
   <title>Badwords</title>
 </head>
 <body>
+
 <?php 
 $par = "lorem ipsum dolor sit amet consectetur adipisicing elit. Qui rem, libero aut dicta quis dolore vel porro facere iusto, hic repudiandae voluptates esse minima aliquid odit enim molestias laboriosam nemo.";
-$badword = $_GET["badword"];
-
+$badWord = ($_GET['badword']);
 ?>
 
 
   <h1>Badwords</h1>
   <p>Paragrafo: <?php echo $par ?></p>
   <p>Lunghezza paragrafo: <?php echo strlen($par); ?>  </p>
-  <p>Parola da censurare: <?php echo $badword ?></p>
-  <p>strpos: <?php echo strpos($par, "lorem"); ?></p>
+  <p>Parola da censurare: <?php echo $badWord ?></p>
+  <p>strpos: <?php echo strpos($par,  $badWord); ?></p>
   <p> <?php 
-    if (strpos($par, "lorem") !== false){
-    echo str_replace("lorem", "*****", $par);
+    if (strpos($par, $badWord) !== false){
+    echo str_replace($badWord, "***", $par);
     } else {
     echo "NO";
     }
